60e131f01ef34fb84bf1e65d643914f0507ee055
yuta24 <yuta24.225@gmail.com>
2026-07-04 00:35:50 +0900

    Initial project

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 6.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FineUIKit",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "FineUIKit",
+            targets: ["FineUIKit"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "FineUIKit",
+            swiftSettings: [
+                .enableUpcomingFeature("ApproachableConcurrency"),
+            ],
+        ),
+        .testTarget(
+            name: "FineUIKitTests",
+            dependencies: ["FineUIKit"],
+            swiftSettings: [
+                .enableUpcomingFeature("ApproachableConcurrency"),
+            ],
+        ),
+    ],
+    swiftLanguageModes: [.v6]
+)
